3cef2a5cc8716db81710090a58afb472cb522068
DarkElement75 <blakeedwards777@gmail.com>
2016-10-26 00:30:29 -0600

    Completely functional with major upgrades

--- dennis5/src/dennis5.py
+++ dennis5/src/dennis5.py
@@ -26,10 +26,10 @@ from bias_inits import *
 
 class Network(object):
 
-    def __init__(self, layers, input_dims, output_dims, cost=cross_entropy, weight_init=glorot_bengio, bias_init=standard):
+    def __init__(self, layers, input_dims, output_dims, cost_type=cross_entropy, weight_init=glorot_bengio, bias_init=standard):
 
         self.layers = layers
-        self.cost = cost
+        self.cost_type = cost_type
         self.weight_init = weight_init
         self.bias_init = bias_init
 
@@ -70,7 +70,10 @@ class Network(object):
     keep_prob is the probability we keep a neuron, the (1 - dropout percentage.)
     """
 
-    def optimize(self, epochs, mb_n, optimization_type='gd', initial_optimization_term1=0.0, optimization_term1_decay_rate=0.0, optimization_term2=0.0, optimization_term3=0.0, optimization_defaults=True, regularization_rate=0.0, keep_prob=1.0):
+    def optimize(self, output_config, epochs, mb_n, optimization_type='gd', initial_optimization_term1=0.0, optimization_term1_decay_rate=0.0, optimization_term2=0.0, optimization_term3=0.0, optimization_defaults=True, regularization_rate=0.0, keep_prob=1.0):
+
+        #Initialize our final output dict for this run
+        output_dict = {}
         
         #Initialize current step number to 0, so we can increment it and use it for our exponential decay
         current_step = tf.Variable(0, trainable=False)
@@ -80,7 +83,7 @@ class Network(object):
         optimization_term1 = tf.train.exponential_decay(initial_optimization_term1, current_step, epochs, optimization_term1_decay_rate, staircase=True)
         
         #Set the value of our cost function passed
-        self.cost = self.cost(self.output, self.y)
+        self.cost = self.cost_type.evaluate(self.output, self.y)
 
         #train_step = tf.train.AdamOptimizer(1e-4).minimize(cross_entropy)
         #train_step = tf.train.GradientDescentOptimizer(learning_rate).minimize(self.cost, global_step=current_step)
@@ -99,14 +102,45 @@ class Network(object):
         self.sess.run(tf.initialize_all_variables())
 
         for step in range(epochs):
+            #For progress
+            if step % 100 == 0:
+                print "\tSTEP %i" % step
+
             batch = mnist.train.next_batch(mb_n)
 
-            #Get our training accuracy.
-            train_accuracy = accuracy.eval(feed_dict={ self.x:batch[0], self.y: batch[1], self.keep_prob: 1.0})
-            print("step %d, training accuracy %g"%(step, train_accuracy))
-
-            #Run training step, with our actual keep prob
+            #Run training step with placeholder values
             train_step.run(feed_dict={self.x: batch[0], self.y: batch[1], self.keep_prob: keep_prob})
 
-            print("test accuracy %g" % accuracy.eval(feed_dict={ self.x: mnist.test.images, self.y: mnist.test.labels, self.keep_prob: 1.0}))
+            #Get our cost
+            step_cost = self.cost.eval(feed_dict={ self.x:batch[0], self.y: batch[1], self.keep_prob: 1.0})
 
+            #Get our various data accuracies
+            training_accuracy = accuracy.eval(feed_dict={ self.x:batch[0], self.y: batch[1], self.keep_prob: 1.0}) * 100
+            validation_accuracy = 0 * 100
+            test_accuracy = accuracy.eval(feed_dict={ self.x: mnist.test.images, self.y: mnist.test.labels, self.keep_prob: 1.0}) * 100
+
+            #Initialize empty list for our outputs at this step
+            output_dict[step] = []
+
+            #Add our output types 
+            #We convert to float because we can't json serialize np.float32, we can only do that with python's float
+            if output_config['output_cost']:
+                output_dict[step].append(float(step_cost))
+            if output_config['output_training_accuracy']:
+                output_dict[step].append(float(training_accuracy))
+            if output_config['output_validation_accuracy']:
+                output_dict[step].append(float(validation_accuracy))
+            if output_config['output_test_accuracy']:
+                output_dict[step].append(float(test_accuracy))
+
+        if output_config['output_cost']:
+            print "\tCost: %f" % (step_cost)
+        if output_config['output_training_accuracy']:
+            print"\tTraining Accuracy: %f%%" % (training_accuracy)
+        if output_config['output_validation_accuracy']:
+            print"\tValidation Accuracy: %f%%" % (validation_accuracy)
+        if output_config['output_test_accuracy']:
+            print"\tTest Accuracy: %f%%" % (test_accuracy)
+        print "\tOPTIMIZATION COMPLETE"
+
+        return output_dict
