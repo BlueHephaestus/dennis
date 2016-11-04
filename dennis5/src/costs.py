fec5029e648f06ed477268cfd27aabce224231a7
DarkElement75 <blakeedwards777@gmail.com>
2016-11-03 21:05:02 -0600

    Fixed cross-entropy comments

--- dennis5/src/costs.py
+++ dennis5/src/costs.py
@@ -4,5 +4,5 @@ class cross_entropy(object):
 
     @staticmethod
     def evaluate(a, y, regularization_term):
-        #We use tf.clip_by_value(tensor, min_value, max_value) to make sure we never do log(0) and thus avoid NaN
+        #We use tf.max(a, 1e-10) to make sure we only clip if we risk doing log(0), thus avoiding NaN problems
         return tf.reduce_mean(-tf.reduce_sum(y * tf.log(tf.maximum(a, 1e-10)), reduction_indices=[1])) + regularization_term
