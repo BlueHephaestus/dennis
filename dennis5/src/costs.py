3e5ecab29f6884a7deb8cf1d173d9d90fb137bd5
DarkElement75 <blakeedwards777@gmail.com>
2016-11-03 21:01:56 -0600

    Fixed NaN possibility with cross-entropy cost

--- dennis5/src/costs.py
+++ dennis5/src/costs.py
@@ -4,4 +4,5 @@ class cross_entropy(object):
 
     @staticmethod
     def evaluate(a, y, regularization_term):
-        return tf.reduce_mean(-tf.reduce_sum(y * tf.log(a), reduction_indices=[1])) + regularization_term
+        #We use tf.clip_by_value(tensor, min_value, max_value) to make sure we never do log(0) and thus avoid NaN
+        return tf.reduce_mean(-tf.reduce_sum(y * tf.log(tf.maximum(a, 1e-10)), reduction_indices=[1])) + regularization_term
